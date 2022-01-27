c4c849708bb6b82c8edc08330653a16b4dd8c59b
Alizasumi <aliza_sumi@hotmail.com>
2022-01-27 00:11:42 -0500

    4th commit

--- src/launchBrowser/GitTest2.java
+++ src/launchBrowser/GitTest2.java
@@ -3,7 +3,10 @@ package launchBrowser;
 public class GitTest2 {
 
 	public static void main(String[] args) {
+		String a ="apple";
 		
+		System.out.println(a.charAt(1));
+		System.out.println(a.substring(1));
 
 	}
 
