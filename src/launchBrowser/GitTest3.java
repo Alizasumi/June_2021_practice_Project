9f25a17fcd542799e3dcfb0850a7666bc9e2fc63
Alizasumi <aliza_sumi@hotmail.com>
2022-01-27 00:17:02 -0500

    last commit

---
+++ src/launchBrowser/GitTest3.java
@@ -0,0 +1,15 @@
+package launchBrowser;
+
+public class GitTest3 {
+
+	public static void main(String[] args) {
+		
+String a ="apple";
+		
+		System.out.println(a.charAt(1));
+		System.out.println(a.substring(1));
+		System.out.println(a.substring(2));
+
+	}
+
+}
